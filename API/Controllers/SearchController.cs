c5c3c43b79f4477f9e9906c6d95919c6c51be847
Dell <2668034168@qq.com>
2020-06-27 12:42:41 +0800

    商品查询API

---
+++ API/Controllers/SearchController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using API.Models;
+using BLL;
+using IBLL;
+using MODEL;
+
+namespace API.Controllers
+{
+    public class SearchController : ApiController
+    {
+        public IProductBLL Bll
+        {
+            get
+            {
+                return new ProductBLL();
+            }
+        }
+
+        public ResponsMessage<List<Product>> Get(string query)
+        {
+            var list = Bll.Search(x => x.ProductTitle.Contains(query));
+            return new ResponsMessage<List<Product>>()
+            {
+                Code = 200,
+                Message = "请求成功",
+                Data = list
+            };
+        }
+    }
+}
